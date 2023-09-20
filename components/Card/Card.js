b34489ae66b6f31233bee5b3c94de5794c3259a7
scottmcook <scott.madison.cook@gmail.com>
2023-09-20 11:59:21 -0700

    Update props to be more generic

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -6,27 +6,25 @@ const UIDB_IMAGE_URL = "https://static.ui.com/fingerprint/ui/icons";
 function Card({ data }) {
   return (
     <>
-      {data.map((cardItem) => {
-        return (
-          <div
-            key={cardItem.id}
-            className="flex flex-col pt-3 border border-[#dbdce1] rounded-lg hover:scale-110 transition duration-200 cursor-pointer object-cover"
-          >
-            <Image
-              className="self-center"
-              alt={cardItem.title}
-              height={150}
-              width={150}
-              src={cardItem.image}
-              style={{ objectFit: "contain", height: 150 }}
-            />
-            <div className="mt-3 ml-3 mb-3">
-              <div>{cardItem.title}</div>
-              <div className="text-sm text-gray-600">{cardItem.category}</div>
-            </div>
-          </div>
-        );
-      })}
+      return (
+      <div
+        key={data.id}
+        className="flex flex-col pt-3 border border-[#dbdce1] rounded-lg hover:scale-110 transition duration-200 cursor-pointer object-cover"
+      >
+        <Image
+          className="self-center"
+          alt={data.title}
+          height={150}
+          width={150}
+          src={data.image}
+          style={{ objectFit: "contain", height: 150 }}
+        />
+        <div className="mt-3 ml-3 mb-3">
+          <div>{data.title}</div>
+          <div className="text-sm text-gray-600">{data.category}</div>
+        </div>
+      </div>
+      );
     </>
   );
 }
