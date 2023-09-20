9d4d1b8f5d571bace5ccf97dd34d2e9df176af8c
scottmcook <scott.madison.cook@gmail.com>
2023-09-20 14:33:54 -0700

    Refactord grid component

--- components/CardGrid/CardGrid.js
+++ components/CardGrid/CardGrid.js
@@ -1,21 +1,12 @@
-// Utilities
-"use client";
 import React, { useReducer, useState } from "react";
+import CountryCard from "../CountryCard/CountryCard";
 
 function CardGrid({ items }) {
   return (
     <>
       <div className="grid grid-cols-4 gap-9">
         {items.map((country) => {
-          return (
-            <div key={country.name} className="">
-              <div className="mt-3 ml-3 mb-3">
-                <div>{country.name}</div>
-                <div>{country.region}</div>
-                <div className="text-sm text-gray-600">{country.capital}</div>
-              </div>
-            </div>
-          );
+          return <CountryCard key={country.alpha3code} data={country} />;
         })}
       </div>
     </>
