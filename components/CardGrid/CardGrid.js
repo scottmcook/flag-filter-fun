d9ef17ba99c7692ddca6dab308f47a4b928154a4
scottmcook <scott.madison.cook@gmail.com>
2023-09-20 14:16:25 -0700

    Refactor to me more generic

--- components/CardGrid/CardGrid.js
+++ components/CardGrid/CardGrid.js
@@ -2,88 +2,22 @@
 "use client";
 import React, { useReducer, useState } from "react";
 
-// Data fetching
-import useSWR from "swr";
-
-// UI Components
-
-import Card from "@/Card/Card";
-import Image from "next/image";
-
-function CardGrid() {
-  const [isTable, setLayout] = useState(false);
-
-  const handleGridToggle = () => {
-    setLayout(false);
-  };
-
-  const handleTableToggle = () => {
-    setLayout(true);
-  };
-
-  const { data, error } = useSWR(handler, fetcher);
-  if (error)
-    return (
-      <div>
-        <h1>404</h1>
-        <p>Loading failed...</p>
-      </div>
-    );
-  if (!data)
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
+function CardGrid({ items }) {
   return (
     <>
-      <div className="flex justify-between border border-y-[#ededf0] px-7">
-        <input
-          className="w-80 my-2 py-2 pl-3 rounded-md background-color--light"
-          placeholder="Search"
-        />
-
-        <div className="flex items-center gap-x-5">
-          <div>
-            <Image
-              alt="list view icon"
-              className={`${
-                isTable ? "background-color--light" : ""
-              } cursor-pointer`}
-              width={20}
-              height={20}
-              src={handleListLogo}
-              onClick={handleTableToggle}
-            />
-          </div>
-          <div>
-            <Image
-              alt="grid view icon"
-              className={`${
-                !isTable ? "background-color--light" : ""
-              } cursor-pointer`}
-              width={20}
-              height={20}
-              src={handleGridLogo}
-              onClick={handleGridToggle}
-            />
-          </div>
-
-          <div>Filter</div>
-        </div>
+      <div className="grid grid-cols-4 gap-9">
+        {items.map((country) => {
+          return (
+            <div key={country.name} className="">
+              <div className="mt-3 ml-3 mb-3">
+                <div>{country.name}</div>
+                <div>{country.region}</div>
+                <div className="text-sm text-gray-600">{country.capital}</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-
-      {!isTable ? (
-        <>
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 mt-6 mx-14">
-            <Card data={data} />
-          </div>
-        </>
-      ) : (
-        <div className="grid mt-6 mx-28 text-neutral-600">
-          <Card data={data} />
-        </div>
-      )}
     </>
   );
 }
