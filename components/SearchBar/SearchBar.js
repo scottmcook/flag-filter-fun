8aa8d9f61c8eeeaf6b6a6b616a376aae09add650
scottmcook <scott.madison.cook@gmail.com>
2023-09-20 12:00:27 -0700

    Add state and event handlers

---
+++ components/SearchBar/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function SearchBar({ searchValue, filterByRegion, onSearch, onFilterChange }) {
+  return (
+    <form>
+      <input
+        type="text"
+        value={searchValue}
+        onChange={(e) => onSearch(e.target.value)}
+        placeholder="Search by country name"
+      />
+      <label for="region-filter">Filter by Region</label>
+      <select
+        name="regions"
+        id="region-filter"
+        onChange={(e) => onFilterChange(e.target.value)}
+      >
+        <option value={"Africa"}>Africa</option>
+        <option value={"America"}>America</option>
+        <option value={"Asia"}>Asia</option>
+        <option value={"Europe"}>Europe</option>
+        <option value={"Oceania"}>Oceania</option>
+      </select>
+      <p>Filtering by {filterByRegion}</p>
+    </form>
+  );
+}
+
+export default SearchBar;
