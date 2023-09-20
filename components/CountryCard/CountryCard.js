f1fe958346bbb89625aebed83123f992140fc187
scottmcook <scott.madison.cook@gmail.com>
2023-09-20 14:33:43 -0700

    Added specificity to card component

---
+++ components/CountryCard/CountryCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+
+const UIDB_IMAGE_URL = "https://static.ui.com/fingerprint/ui/icons";
+
+function CountryCard({ data }) {
+  const { name, flag, capital, population, region } = data;
+  return (
+    <>
+      <div className="flex flex-col pt-3 border border-[#dbdce1] rounded-lg hover:scale-110 transition duration-200 cursor-pointer object-cover">
+        <Image
+          className="self-center"
+          alt={`${name} flag`}
+          height={150}
+          width={150}
+          src={flag}
+          style={{ objectFit: "contain", height: 150 }}
+        />
+        <div className="mt-3 ml-3 mb-3">
+          <div>{name}</div>
+          <div>{capital}</div>
+          <div>{population}</div>
+          <div>{region}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CountryCard;
