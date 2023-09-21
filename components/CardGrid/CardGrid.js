397dce9295e3242e3bea633d2bf3bc5c15b85cf2
scottmcook <scott.madison.cook@gmail.com>
2023-09-20 20:42:32 -0700

    Remove unused import

--- components/CardGrid/CardGrid.js
+++ components/CardGrid/CardGrid.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from "react";
+import React, { useState } from "react";
 import CountryCard from "../CountryCard/CountryCard";
 
 function CardGrid({ items, searchValue }) {
