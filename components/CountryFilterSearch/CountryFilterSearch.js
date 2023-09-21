bddb9d05961d7e4585e0480938bfb5fafe75e15c
scottmcook <scott.madison.cook@gmail.com>
2023-09-20 21:27:58 -0700

    Add default state

--- components/CountryFilterSearch/CountryFilterSearch.js
+++ components/CountryFilterSearch/CountryFilterSearch.js
@@ -3,7 +3,7 @@ import SearchBar from "@/components/SearchBar/SearchBar";
 import CardGrid from "@/components/CardGrid/CardGrid";
 
 function CountryFilterSearch({ countries }) {
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("Search for a country...");
   const [filterByRegion, setFilterByRegion] = useState("");
 
   return (
