d646e7b63d2f7e717f6d9a313de8eabd7e8fb67b
scottmcook <scott.madison.cook@gmail.com>
2023-09-27 15:43:55 -0700

    Add filter and search filter functions

--- components/CardGrid/CardGrid.js
+++ components/CardGrid/CardGrid.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import CountryCard from "../CountryCard/CountryCard";
 
-function CardGrid({ items, searchValue }) {
+function CardGrid({ items, searchValue, filterByRegion }) {
+  let loadItems = items.map((country) => {
+    return <CountryCard key={country.name} data={country} />;
+  });
+  const searchMatch = (value) => value.country == searchValue;
+  const filterMatch = (value) => value.region == filterByRegion;
+
+  const searchItem = loadItems.filter(searchMatch);
+  const filteredItems = loadItems.filter(filterMatch);
+
   return (
     <>
-      <div className="grid grid-cols-4 gap-20">
-        {items.map((country) => {
-          if (searchValue == country.name) {
-            return <CountryCard key={country} data={country} />;
-          } else if (searchValue == "") {
-            return <CountryCard key={country.alpha3code} data={country} />;
-          }
-        })}
-      </div>
+      <div className="grid grid-cols-4 gap-20">{loadItems}</div>
     </>
   );
 }
