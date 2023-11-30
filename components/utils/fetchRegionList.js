dbb382f3ac58bcc68b5921aaca005166ca628f26
Scott Cook <scott.madison.cook@gmail.com>
2023-11-30 08:03:40 -0600

    Add react-query

---
+++ components/utils/fetchRegionList.js
@@ -0,0 +1,17 @@
+async function fetchRegionList({ queryKey }) {
+  const animal = queryKey[1];
+
+  if (!animal) return [];
+
+  const res = await fetch(
+    `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
+
+  if (!res.ok) {
+    throw new Error(`breeds ${animal} fetch not ok`);
+  }
+
+  return res.json();
+}
+
+export default fetchRegionList;
