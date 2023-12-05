f353b007deb411cb26dd74d5711fb8e5b672808e
Scott Cook <scott.madison.cook@gmail.com>
2023-12-05 12:18:27 -0800

    Move state to higher order components

--- components/CardGrid/CardGrid.js
+++ components/CardGrid/CardGrid.js
@@ -1,25 +1,27 @@
 import React, { useMemo, useState } from "react";
 import CountryCard from "@/components/CountryCard/CountryCard";
 
-function CardGrid({ items, searchValue, filterByRegion }) {
-  const searchMatch = (value) => value.country == searchValue;
-  const filterMatch = (value) => {
-    return value.region == filterByRegion;
-  };
-
-  const searchItem = items.filter(searchMatch);
-  const filteredCountries = items.filter(filterMatch).map((country) => {
-    return <CountryCard key={country.name} country={country} />;
-  });
-
-  const loadAllCountries = items.map((country) => {
-    return <CountryCard key={country.name} country={country} />;
-  });
-
+function CardGrid({ countries }) {
   return (
     <>
       <div className="grid grid-cols-4 gap-20">
-        {filteredCountries.length > 1 ? filteredCountries : loadAllCountries}
+      {!countries.length ? (
+        <h1>No countries found</h1>
+      ) : (
+        countries.map((pet) => {
+          return (
+            <CountryCard
+              animal={pet.animal}
+              id={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={pet.city}
+              key={pet.id}
+            />
+          );
+        })
+      )}
       </div>
     </>
   );
---
+++ components/SearchParams/SearchParams.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchSearch from "../utils/fetchSearch";
+import CardGrid from "../CardGrid/CardGrid";
+
+const REGIONS = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+
+const SearchParams = () => {
+  const [requestParam, setRequestParam] = useState({
+    name: "",
+    capital: "",
+  });
+
+  const results = useQuery({ requestParam, fetchSearch});
+  const countries = results?.data?.name ?? [];
+  
+  return (
+    <div>
+
+    <form
+        className="flex justify-between pb-10"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          const obj = {
+            animal: formData.get("animal") ?? "",
+            breed: formData.get("breed") ?? "",
+            location: formData.get("location") ?? "",
+          };
+          setRequestParams(obj);
+        }}
+      >
+      <label htmlFor="country">
+        Country Search 
+      <input
+        id="country"
+        name="country"
+        placeholder="Country"
+        />
+      </label>
+      <div>
+        <label className="justify-self-end" htmlfor="region-filter">
+          Filter by Region
+        </label>
+        <select
+          name="regions"
+          id="region-filter"
+          placeholder="Search for a country..."
+          onChange={(e) => onFilterChange(e.target.value)}
+          >
+          {REGIONS.map(region => {
+            return (
+              <option key={region} value={region}>{region}</option>
+              )
+            })}
+        </select>
+      </div>
+    </form>
+      <CardGrid countries={countries} />
+    </div>
+  );
+}
+
+export default SearchParams;
