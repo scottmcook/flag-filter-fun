89a55365927ce8018babaa78e3b7a0826e808075
Scott Cook <scott.madison.cook@gmail.com>
2023-12-06 20:35:35 -0800

    Add propnames

--- components/CountryCard/CountryCard.js
+++ components/CountryCard/CountryCard.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 
-function CountryCard({ country }) {
-  const { name, flag, capital, population, region } = country;
+function CountryCard({ name, flag, capital, population, region }) {
+  // const { name, flag, capital, population, region } = country;
   return (
     <>
       <div className="flex flex-col pt-3 border border-[#dbdce1] rounded-lg hover:scale-110 transition duration-200 cursor-pointer object-cover">
