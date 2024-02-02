0d01ef67015935fb2dfe5bff173a77db432038bb
Scott Cook <scott.madison.cook@gmail.com>
2024-02-01 17:07:40 -0800

    Add react-query to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import NavBar from './components/NavBar/NavBar'
 import SearchParams from './components/SearchParams/SearchParams'
+import { QueryClient, QueryClientProvider } from 'react-query'
 // import './App.css';
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <NavBar />
       <SearchParams />
-    </>
+    </QueryClientProvider>
   )
 }
 
