4a32b7eee4dac9c82fb7de9cf3a774c10325ef85
Scott Cook <scott.madison.cook@gmail.com>
2024-02-03 11:33:04 -0800

    Update fonts

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -8,17 +8,17 @@ type CountryCardProps = {
 function CountryCard({ name, flag, capital, population, region }: CountryCardProps) {
   return (
     <>
-      <div className="flex flex-col pt-3 border border-[#dbdce1] rounded-lg hover:scale-110 transition duration-200 cursor-pointer object-cover">
+      <div className="flex flex-col pt-3 w-56 overflow-hidden shadow-md dark:bg-dark-blue-elements rounded-lg hover:scale-110 transition duration-200 cursor-pointer object-contain ">
         <img
-          className="w-full h-auto self-center"
+          className="w-full  scale-125"
           src={flag}
           alt={`${name} flag`}
         />
-        <div className="mt-3 ml-3 mb-3">
-          <div><span className="font-semibold">Name:</span> {name}</div>
-          <div><span className="font-semibold">Capital:</span> {capital}</div>
+        <div className="text-sm mt-3 ml-4 pb-10">
+          <div className="text-lg font-bold pb-4">{name}</div>
           <div><span className="font-semibold">Population:</span> {population.toLocaleString('en-US')}</div>
           <div><span className="font-semibold">Region:</span> {region}</div>
+          <div><span className="font-semibold">Capital:</span> {capital}</div>
         </div>
       </div>
     </>
