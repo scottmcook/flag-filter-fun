bd0091c771bbe17ace5bb75e33998dec27a64856
Scott Cook <scott.madison.cook@gmail.com>
2024-02-03 15:25:09 -0800

    Add moon icon

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -30,7 +30,10 @@ function NavBar() {
   return (
     <nav className="flex justify-between px-8 py-5 mb-10 bg-white dark:bg-dark-blue-elements shadow-sm">
       <h1 className="text-3xl  font-bold">Where in the World?</h1>
-      <button className="" onClick={() => toggleTheme()}>Dark Mode</button>
+      <button className="flex justify-center items-center" onClick={() => toggleTheme()}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="inline dark:fill-white w-5 h-5 pr-1">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" /></svg>
+{isDarkTheme ? 'Light': 'Dark'} Mode</button>
     </nav>
   );
 }
