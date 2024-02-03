71835bc5eb4d83194b3fb392cd4f907333c6db59
Scott Cook <scott.madison.cook@gmail.com>
2024-02-03 14:48:04 -0800

    Update API url

--- src/components/SearchParams/SearchParams.tsx
+++ src/components/SearchParams/SearchParams.tsx
@@ -26,7 +26,7 @@ const SearchParams = () => {
     "countries",
     async () => {
       const response = await fetch(
-        "https://raw.githubusercontent.com/iamspruce/search-filter-painate-reactjs/main/data/countries.json"
+        "https://raw.githubusercontent.com/scottmcook/flag-filter-fun/main/data/items.json"
       );
       const result = await response.json();
       return Object.values(result) as Country[];
@@ -51,38 +51,37 @@ const SearchParams = () => {
 
   return (
     <div>
-      <label htmlFor="country" className="sr-only">
-        Country Search 
-      </label>
-      <input
-        id="country"
-        name="country"
-        placeholder="Search for a country"
-        className="py-3 dark:bg-gray-700 shadow-md rounded-md"
-        onChange={(e) => setSearchQuery(e.target.value)}
-        />
-  
-      <div>
-        <label className="justify-self-end" htmlFor="region-filter">
-          Filter by Region
-        <select
-          name="regions"
-          id="region-filter"
-          className="dark:bg-gray-700"
-          onChange={(e) => {
-            setFilterParam(e.target.value);
-          }}
-          >
-          {REGIONS.map(region => {
-            return (
-              <option key={region} value={region}>{region}</option>
-              )
-            })}
-        </select>
+      <div className="flex justify-between mx-8 mb-10">
+        <label htmlFor="country" className="sr-only">
+          Country Search 
         </label>
+        <input
+          id="country"
+          name="country"
+          placeholder="Search for a country"
+          className="w-1/3 pl-5 py-3 shadow-sm rounded-md dark:bg-dark-blue-elements dark:font-white "
+          onChange={(e) => setSearchQuery(e.target.value)}
+          />
+
+      
+          <label className="sr-only" htmlFor="region-filter">
+            Filter by Region
+          </label>
+          <select
+            name="regions"
+            id="region-filter"
+            className="w-48 pl-2 shadow-sm rounded-md dark:bg-dark-blue-elements"
+            onChange={(e) => {
+              setFilterParam(e.target.value);
+            }}
+            >
+            {REGIONS.map(region => {
+              return (
+                <option key={region} value={region}>{region}</option>
+                )
+              })}
+          </select>
       </div>
-
-
 
       {countries ? (
            <CardGrid countries={filteredData} />
