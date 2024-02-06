a62c115302e65ca4eff75e13830a4cbb3950603a
Scott Cook <scott.madison.cook@gmail.com>
2024-02-06 09:34:25 -0800

    Add react router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from 'react-query'
 import NavBar from './components/NavBar/NavBar'
 import SearchParams from './components/SearchParams/SearchParams'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import Details from "./components/Details/Details";
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className='bg-light-gray text-very-dark-blue-font dark:text-white dark:bg-dark-very-blue '>
-        <NavBar />
-        <SearchParams />
-      </div>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <div className='bg-light-gray text-very-dark-blue-font dark:text-white dark:bg-dark-very-blue '>
+          <NavBar />
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+        </div>
+      </QueryClientProvider>
+    </BrowserRouter>
   )
 }
 
